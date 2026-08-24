2080b9cddebd74434f30b6c81d1870bb1f5c196b
Oscar Polania <opolania11@gmail.com>
2026-08-24 13:37:55 -0500

    Add files via upload

---
+++ marca.js
@@ -0,0 +1,15 @@
+/* Único archivo que se toca al publicar. */
+window.MARCA = {
+  /* Pega aquí la URL /exec que te da Apps Script al implementar el backend */
+  API_URL: 'PEGA_AQUI_LA_URL_DEL_BACKEND',
+
+  /* Respaldo: se usa solo mientras el servidor responde (primer pintado) */
+  EVENTO_NOMBRE: 'FESTIVAL DE COMETAS XVII VIENTOS DE PAZ',
+  EVENTO_LEMA: 'FAMILIAS UNIDAS PARA TRANSFORMAR A FLANDES',
+  EVENTO_HOMENAJE: 'Homenaje a nuestra querida amiga Beatriz Aya Hurtado',
+  EVENTO_LUGAR: 'Cancha de fútbol del barrio La Ceiba',
+  EVENTO_MAPS: 'https://maps.app.goo.gl/BvUJ78E6hD7EYLyc6',
+  EVENTO_LAT: '4.2834234',
+  EVENTO_LNG: '-74.8182405',
+  EVENTO_INICIO_ISO: '2026-08-20T15:00:00-05:00'
+};
